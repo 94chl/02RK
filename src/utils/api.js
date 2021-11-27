006b7760f024003d22c66299e87e7aafb2f882ed
Yongrok <94chl@naver.com>
2021-11-27 17:50:01 +0900

    배포용 빌드 파일 추가

--- src/utils/api.js
+++
@@ -1,8 +0,0 @@
-export const request = (data) => {
-  return fetch(`./json/${data}.json`)
-    .then((res) => {
-      if (res.ok) return res.json();
-      throw new Error(`${res.status} Error`);
-    })
-    .catch((e) => alert(__dirname));
-};

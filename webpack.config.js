d4542fb81556d2a87d56277895d5a8d6dfb6eb6e
Yongrok <94chl@naver.com>
2022-02-06 22:06:01 +0900

    github actions 도입

--- webpack.config.js
+++ webpack.config.js
@@ -18,7 +18,7 @@ module.exports = {
   entry: "./src/main.js",
   output: {
     path: path.resolve(__dirname, "dist"),
-    publicPath: "/",
+    publicPath: "./",
     clean: true,
   },
   module: {

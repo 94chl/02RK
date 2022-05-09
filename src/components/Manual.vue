5e60604523e5e8496686484f71bacf6afd96643d
94chl <94chl@naver.com>
2022-05-09 10:26:28 +0900

    팁 추가 및 아이콘 변경

--- src/components/Manual.vue
+++ src/components/Manual.vue
@@ -15,88 +15,89 @@
         <li>{{ $t("manual.manual2") }}</li>
         <li>{{ $t("manual.manual3") }}</li>
         <li>{{ $t("manual.manual4") }}</li>
+        <li>{{ $t("manual.manual5") }}</li>
       </ul>
     </div>
   </div>
 </template>
 
 <script>
-  export default {
-    components: {},
-    computed: {
-      toggleModal() {
-        return this.$store.state.toggleModal;
-      },
+export default {
+  components: {},
+  computed: {
+    toggleModal() {
+      return this.$store.state.toggleModal;
     },
-    methods: {
-      toggleManualModal() {
-        this.$store.dispatch("onToggleModal", "manual");
-      },
+  },
+  methods: {
+    toggleManualModal() {
+      this.$store.dispatch("onToggleModal", "manual");
     },
-  };
+  },
+};
 </script>
 
 <style lang="scss" scoped>
-  .manual {
-    border-radius: 5px;
-    border: 2px solid $color3;
-    box-sizing: border-box;
-    background: $color2;
+.manual {
+  border-radius: 5px;
+  border: 2px solid $color3;
+  box-sizing: border-box;
+  background: $color2;
 
-    &.active {
-      @include active();
-      z-index: 25;
-    }
+  &.active {
+    @include active();
+    z-index: 25;
+  }
 
-    .tabName {
-      color: $color2;
+  .tabName {
+    color: $color2;
+    height: 25px;
+    line-height: 25px;
+    margin: 5px 0;
+    padding: 0 5px;
+    display: grid;
+    grid-template-columns: auto 1fr;
+    align-items: center;
+    h3 {
+      color: $color3;
       height: 25px;
       line-height: 25px;
-      margin: 5px 0;
-      padding: 0 5px;
-      display: grid;
-      grid-template-columns: auto 1fr;
-      align-items: center;
-      h3 {
-        color: $color3;
-        height: 25px;
-        line-height: 25px;
-        padding: 0;
-        margin: 0;
-      }
-      .buttonBox {
-        width: 100%;
-        display: flex;
-        align-items: center;
-        justify-content: flex-end;
-
-        > div {
-          display: flex;
-        }
-
-        button {
-          background: none;
-          color: $color3;
-          border-radius: 5px;
-          @include fasIcon(25px);
-          margin-right: 5px;
-          &:hover {
-            box-shadow: 0 0 12px 2px inset rgba(0, 0, 0, 0.3);
-          }
-        }
-      }
+      padding: 0;
+      margin: 0;
     }
+    .buttonBox {
+      width: 100%;
+      display: flex;
+      align-items: center;
+      justify-content: flex-end;
 
-    &_info {
-      ul {
-        background: $color3;
-        padding: 8px;
-        list-style: disc inside;
-        li {
-          color: $color2;
-          padding: 4px;
+      > div {
+        display: flex;
+      }
+
+      button {
+        background: none;
+        color: $color3;
+        border-radius: 5px;
+        @include fasIcon(25px);
+        margin-right: 5px;
+        &:hover {
+          box-shadow: 0 0 12px 2px inset rgba(0, 0, 0, 0.3);
         }
       }
     }
   }
+
+  &_info {
+    ul {
+      background: $color3;
+      padding: 8px;
+      list-style: disc inside;
+      li {
+        color: $color2;
+        padding: 4px;
+      }
+    }
+  }
+}
 </style>
--- src/components/Status.vue
+++ src/components/Status.vue
@@ -12,7 +12,7 @@
                 }`"
                 @click="onChangeStatusData"
               >
-                <span> <i class="fas fa-shopping-cart"></i> </span>
+                <span> <i class="fa-solid fa-crosshairs"></i> </span>
               </button>
               <button
                 :class="`changeStatusDataBtn ${
@@ -179,248 +179,247 @@
 </template>
 
 <script>
-  import { searchById, itemOptions } from "~/utils/itemTable";
+import { searchById, itemOptions } from "~/utils/itemTable";
 
-  export default {
-    data() {
-      return {
-        commonOptions: [
-          "id",
-          "sort",
-          "img",
-          "location",
-          "tobe",
-          "material",
-          "pickup",
-          "limit",
-          "stack",
-          "name",
-          "reload",
-        ],
-        itemOptions,
-        statusData: "targetItems",
-      };
+export default {
+  data() {
+    return {
+      commonOptions: [
+        "id",
+        "sort",
+        "img",
+        "location",
+        "tobe",
+        "material",
+        "pickup",
+        "limit",
+        "stack",
+        "name",
+        "reload",
+      ],
+      itemOptions,
+      statusData: "targetItems",
+    };
+  },
+  components: {},
+  computed: {
+    language() {
+      return this.$store.state.language;
     },
-    components: {},
-    computed: {
-      language() {
-        return this.$store.state.language;
-      },
-      toggleModal() {
-        return this.$store.state.toggleModal;
-      },
-      showItemImg() {
-        return this.$store.state.showItemImg;
-      },
-      targetItems() {
-        return this.$store.state.targetItems;
-      },
-      bagItems() {
-        return this.$store.state.bagEquip;
-      },
-      equipOptions() {
-        const equipData =
-          this.statusData === "targetItems" ? this.targetItems : this.bagItems;
-        const equipOptions = Object.values(equipData).reduce(
-          (itemOptions, item) => {
-            if (item.id) {
-              const itemInfo = searchById(item.id);
-              Object.entries(itemInfo).forEach(([optionId, optionValue]) => {
-                if (!this.commonOptions.includes(optionId)) {
-                  if (optionId.match(/(active)|(buff)|(uniq)/g)) {
-                    itemOptions[optionId] = !itemOptions[optionId]
-                      ? optionValue
-                      : optionValue > itemOptions[optionId]
-                      ? optionValue
-                      : itemOptions[optionId];
-                  } else {
-                    itemOptions[optionId] = !itemOptions[optionId]
-                      ? optionValue
-                      : itemOptions[optionId] + optionValue;
-                  }
+    toggleModal() {
+      return this.$store.state.toggleModal;
+    },
+    showItemImg() {
+      return this.$store.state.showItemImg;
+    },
+    targetItems() {
+      return this.$store.state.targetItems;
+    },
+    bagItems() {
+      return this.$store.state.bagEquip;
+    },
+    equipOptions() {
+      const equipData =
+        this.statusData === "targetItems" ? this.targetItems : this.bagItems;
+      const equipOptions = Object.values(equipData).reduce(
+        (itemOptions, item) => {
+          if (item.id) {
+            const itemInfo = searchById(item.id);
+            Object.entries(itemInfo).forEach(([optionId, optionValue]) => {
+              if (!this.commonOptions.includes(optionId)) {
+                if (optionId.match(/(active)|(buff)|(uniq)/g)) {
+                  itemOptions[optionId] = !itemOptions[optionId]
+                    ? optionValue
+                    : optionValue > itemOptions[optionId]
+                    ? optionValue
+                    : itemOptions[optionId];
+                } else {
+                  itemOptions[optionId] = !itemOptions[optionId]
+                    ? optionValue
+                    : itemOptions[optionId] + optionValue;
                 }
-              });
-            }
-            return itemOptions;
-          },
-          {}
-        );
+              }
+            });
+          }
+          return itemOptions;
+        },
+        {}
+      );
 
-        return equipOptions;
-      },
+      return equipOptions;
     },
-    methods: {
-      onChangeShowItemImg() {
-        this.$store.dispatch("onChangeShowItemImg");
-      },
-      toggleStatusModal() {
-        this.$store.dispatch("onToggleModal", "status");
-      },
-      onChangeStatusData() {
-        const nextStatusData = ["targetItems", "bagItems"].filter(
-          (dataType) => this.statusData !== dataType
-        )[0];
-        this.statusData = nextStatusData;
-      },
-      showItemInfo(e) {
-        const selectedItem = searchById(e.target.closest("li").dataset.itemid);
-        this.$store.dispatch("setCart", selectedItem);
-      },
+  },
+  methods: {
+    onChangeShowItemImg() {
+      this.$store.dispatch("onChangeShowItemImg");
     },
-  };
+    toggleStatusModal() {
+      this.$store.dispatch("onToggleModal", "status");
+    },
+    onChangeStatusData() {
+      const nextStatusData = ["targetItems", "bagItems"].filter(
+        (dataType) => this.statusData !== dataType
+      )[0];
+      this.statusData = nextStatusData;
+    },
+    showItemInfo(e) {
+      const selectedItem = searchById(e.target.closest("li").dataset.itemid);
+      this.$store.dispatch("setCart", selectedItem);
+    },
+  },
+};
 </script>
 
 <style lang="scss" scoped>
-  .status {
-    border-radius: 5px;
-    border: 2px solid $color3;
-    box-sizing: border-box;
-    background: $color2;
+.status {
+  border-radius: 5px;
+  border: 2px solid $color3;
+  box-sizing: border-box;
+  background: $color2;
 
-    &.active {
-      @include active();
-    }
+  &.active {
+    @include active();
+  }
 
+  .tabName {
+    color: $color2;
+    height: 25px;
+    line-height: 25px;
+    margin: 5px 0;
+    padding: 0 5px;
+  }
+
+  &_equip {
     .tabName {
-      color: $color2;
-      height: 25px;
-      line-height: 25px;
-      margin: 5px 0;
-      padding: 0 5px;
-    }
-
-    &_equip {
-      .tabName {
-        display: grid;
-        grid-template-columns: auto 1fr;
+      display: grid;
+      grid-template-columns: auto 1fr;
+      align-items: center;
+      h3 {
+        color: $color3;
+        height: 25px;
+        line-height: 25px;
+        padding: 0;
+        margin: 0;
+      }
+      .buttonBox {
+        width: 100%;
+        display: flex;
         align-items: center;
-        h3 {
-          color: $color3;
-          height: 25px;
-          line-height: 25px;
-          padding: 0;
-          margin: 0;
-        }
-        .buttonBox {
-          width: 100%;
+        justify-content: space-between;
+
+        > div {
           display: flex;
-          align-items: center;
-          justify-content: space-between;
+        }
 
-          > div {
-            display: flex;
+        button {
+          background: none;
+          color: $color3;
+          border-radius: 5px;
+          @include fasIcon(25px);
+          margin-right: 5px;
+          &:last-child {
+            margin: 0;
           }
-
-          button {
-            background: none;
-            color: $color3;
-            border-radius: 5px;
-            @include fasIcon(25px);
-            margin-right: 5px;
-            &:last-child {
-              margin: 0;
-            }
-            &:hover {
-              box-shadow: 0 0 12px 2px inset rgba(0, 0, 0, 0.3);
-            }
-            &.selected {
-              box-shadow: 0 0 12px 2px inset rgba(0, 0, 0, 0.3);
-            }
+          &:hover {
+            box-shadow: 0 0 12px 2px inset rgba(0, 0, 0, 0.3);
+          }
+          &.selected {
+            box-shadow: 0 0 12px 2px inset rgba(0, 0, 0, 0.3);
           }
         }
       }
+    }
 
-      .targetItems {
-        display: grid;
-        grid-template-columns: repeat(6, 1fr);
-        @media screen and (max-width: 720px) {
-          grid-template-columns: repeat(3, 1fr);
+    .targetItems {
+      display: grid;
+      grid-template-columns: repeat(6, 1fr);
+      @media screen and (max-width: 720px) {
+        grid-template-columns: repeat(3, 1fr);
+      }
+
+      .itemInfo {
+        background: #fff;
+        border-radius: 5px;
+        margin: 5px;
+
+        .showItemInfoBtn {
+          border-radius: 5px;
+          &:hover {
+            box-shadow: 0 0 12px 2px inset rgba(0, 0, 0, 0.2);
+          }
         }
+
+        div {
+          padding: 5px;
+          text-align: center;
+        }
+      }
+    }
+
+    .bagItems {
+      display: grid;
+      grid-template-columns: repeat(3, 1fr);
+      grid-template-rows: repeat(2, 1fr);
+      gap: 10px;
+      background: $color3;
+      border-radius: 5px;
+      padding: 5px;
+      li {
+        height: 25px;
+        line-height: 25px;
+        position: relative;
+        background: #fff;
+        border-radius: 5px;
 
         .itemInfo {
-          background: #fff;
-          border-radius: 5px;
-          margin: 5px;
+          @include fasIcon(25px);
+          width: 100%;
+          overflow: hidden;
+          font-size: 0.8em;
+          border: none;
 
-          .showItemInfoBtn {
-            border-radius: 5px;
-            &:hover {
-              box-shadow: 0 0 12px 2px inset rgba(0, 0, 0, 0.2);
-            }
-          }
-
-          div {
-            padding: 5px;
-            text-align: center;
+          img {
+            height: 100%;
+            width: fit-content;
           }
         }
-      }
 
-      .bagItems {
-        display: grid;
-        grid-template-columns: repeat(3, 1fr);
-        grid-template-rows: repeat(2, 1fr);
-        gap: 10px;
-        background: $color3;
-        border-radius: 5px;
-        padding: 5px;
-        li {
-          height: 25px;
-          line-height: 25px;
-          position: relative;
-          background: #fff;
-          border-radius: 5px;
-
-          .itemInfo {
-            @include fasIcon(25px);
-            width: 100%;
-            overflow: hidden;
-            font-size: 0.8em;
-            border: none;
-
-            img {
-              height: 100%;
-              width: fit-content;
-            }
-          }
-
-          .empty {
-            color: #999;
-            font-size: 0.7em;
-            padding: 5px;
-          }
+        .empty {
+          color: #999;
+          font-size: 0.7em;
+          padding: 5px;
         }
       }
     }
+  }
 
-    &_info {
-      .optionBox {
-        border-radius: 5px;
-        .option {
-          .optionInfo {
-            display: flex;
-            background: $color3;
-            color: $color2;
-            padding: 5px;
-            &.unique {
-              display: block;
-            }
-            &_name {
-              font-weight: 700;
-            }
-            &_value {
-              margin-left: 4px;
-            }
-            &_values {
-              word-break: keep-all;
+  &_info {
+    .optionBox {
+      border-radius: 5px;
+      .option {
+        .optionInfo {
+          display: flex;
+          background: $color3;
+          color: $color2;
+          padding: 5px;
+          &.unique {
+            display: block;
+          }
+          &_name {
+            font-weight: 700;
+          }
+          &_value {
+            margin-left: 4px;
+          }
+          &_values {
+            word-break: keep-all;
 
-              .uniqueOptions {
-                list-style: disc outside;
-                padding: 4px;
+            .uniqueOptions {
+              list-style: disc outside;
+              padding: 4px;
 
-                li {
-                  margin: 4px 0;
-                }
+              li {
+                margin: 4px 0;
               }
             }
           }
@@ -428,4 +427,5 @@
       }
     }
   }
+}
 </style>
